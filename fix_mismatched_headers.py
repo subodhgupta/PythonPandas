e93c4389a6a94db3d5fd6513e020f84b79ef90ab
subodhgupta <subodhgupta@users.noreply.github.com>
2020-02-13 11:38:59 -0500

    Initial Commit.

--- fix_mismatched_headers.py
+++ fix_mismatched_headers.py
@@ -1,24 +1,29 @@
+# Read excel files with mismatched column names and even incorrectly mapped columns
+# For example, a column can be called "Student Name" in one file and "Student NM' in another file
+# Also, "Student Name" can be the first column in one file but the third column in second file
+# This program can read a mapping file and consolidate the mismatched data.
 # import dependencies
-import pandas as pd
+from pandas import DataFrame, read_excel, ExcelWriter
 import os
-import util as u
-from importlib import reload as rl
+import util
 
 # Get the folder name to build filepath
 folder = 'static'
+consolidated_workbook = 'all_students'
 
-column_connections = u.retrieve_column_connection(os.path.join('static', 'column_connections.xlsx'))
-worksheets = u.distinct_workbook_worksheet(column_connections)
+column_connections = util.retrieve_column_connection(os.path.join(folder, 'mapping_file.xlsx'))
+worksheets = util.distinct_workbook_worksheet(column_connections)
+
+print(worksheets)
 
 # Create dataframe to store final result
-df_all_students = pd.DataFrame()
+df_all_students = DataFrame()
 
 # Iterate over dataframe using the itertuples method (Use Pandas documentation as reference)
 for workbook, worksheet in worksheets.itertuples(index=False):
-
     # Filter workbook/worksheet and store the result into a dataframe
     columns_detail = column_connections[(column_connections['workbook'] == workbook) &
-                       (column_connections['worksheet'] == worksheet)]
+                                        (column_connections['worksheet'] == worksheet)]
 
     # Create column id list - we will use as a parameter to read_excel method below
     columns_id_list = columns_detail['column_id'].to_list()
@@ -35,18 +40,22 @@ for workbook, worksheet in worksheets.itertuples(index=False):
 
     # Build file path using os.path.join method
     filepath = os.path.join(folder, workbook + '.xlsx')
-
+    # columns_rename_list.sort(key=dict(zip(columns_rename_list, columns_id_list)).get)
     # Fill partial result using read_excel to read this specific workbook
-    partial_result = pd.read_excel(filepath,
-                                   sheet_name=worksheet,
-                                   usecols=columns_id_list,
-                                   names=columns_rename_list)
+    partial_result = read_excel(filepath,
+                                sheet_name=worksheet,
+                                usecols=columns_id_list,
+                                names=columns_rename_list)
 
     # show the partial results
     print(partial_result)
 
     df_all_students = df_all_students.append(partial_result, ignore_index=True)
+    print(df_all_students)
 
+consolidated_workbook_filepath = os.path.join(folder, consolidated_workbook + '.xlsx')
+writer = ExcelWriter(consolidated_workbook_filepath)
+df_all_students.to_excel(writer, consolidated_workbook, index=False)
+writer.save()
 
-# refências:
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.itertuples.html#pandas.DataFrame.itertuples
